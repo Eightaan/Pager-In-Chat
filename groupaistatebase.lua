4435e970e1822b0d4341bdbe4b0623542d329f11
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-09-10 15:59:37 +0200

    Add files via upload

--- groupaistatebase.lua
+++ groupaistatebase.lua
@@ -1,17 +1,33 @@
-local _f_load = GroupAIStateBase.load
-function GroupAIStateBase:load(...)
-    _f_load(self, ...)
-    PagerChat:sync_pagers(self._nr_successful_alarm_pager_bluffs)
-end
-
-local _f_on_successful_alarm_pager_bluff = GroupAIStateBase.on_successful_alarm_pager_bluff
+--Host
+local GroupAIStateBase_on_successful_alarm_pager_bluff = GroupAIStateBase.on_successful_alarm_pager_bluff
 function GroupAIStateBase:on_successful_alarm_pager_bluff()
-    _f_on_successful_alarm_pager_bluff(self)
-    PagerChat:add_pager()
+    GroupAIStateBase_on_successful_alarm_pager_bluff(self)
+	local pagers_used = self._nr_successful_alarm_pager_bluffs
+	local max_pagers_data = managers.player:has_category_upgrade("player", "corpse_alarm_pager_bluff") and tweak_data.player.alarm_pager.bluff_success_chance_w_skill or tweak_data.player.alarm_pager.bluff_success_chance
+	local max_num_pagers = #max_pagers_data
+
+	for i, chance in ipairs(max_pagers_data) do
+		if chance == 0 then
+			max_num_pagers = i - 1
+			break
+		end
+	end
+    managers.chat:_receive_message(ChatManager.GAME,"Pagers Answered", pagers_used.."/"..max_num_pagers, Color('29A4F6'))
 end
 
-local _f_sync_alarm_pager_bluff = GroupAIStateBase.sync_alarm_pager_bluff
+--Client
+local GroupAIStateBase_sync_alarm_pager_bluff = GroupAIStateBase.sync_alarm_pager_bluff
 function GroupAIStateBase:sync_alarm_pager_bluff()
-    _f_sync_alarm_pager_bluff(self)
-    PagerChat:add_pager()
+    GroupAIStateBase_sync_alarm_pager_bluff(self)
+	local pagers_used = self._nr_successful_alarm_pager_bluffs
+	local max_pagers_data = managers.player:has_category_upgrade("player", "corpse_alarm_pager_bluff") and tweak_data.player.alarm_pager.bluff_success_chance_w_skill or tweak_data.player.alarm_pager.bluff_success_chance
+	local max_num_pagers = #max_pagers_data
+
+	for i, chance in ipairs(max_pagers_data) do
+		if chance == 0 then
+			max_num_pagers = i - 1
+			break
+		end
+	end
+    managers.chat:_receive_message(ChatManager.GAME,"Pagers Answered", pagers_used.."/"..max_num_pagers, Color('29A4F6'))
 end
