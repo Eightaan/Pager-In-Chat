9abbdd03963fac059f078a6c349ff3aaf9fdac85
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-09-10 07:58:38 +0200

    Update menumanager.lua

--- menumanager.lua
+++ menumanager.lua
@@ -1,17 +1,21 @@
 if not _G.PagerChat then
 	_G.PagerChat = {}
-	local sync = true  -- Set to false if you don't want your whole team to read the messages
-
-    local Net = _G.LuaNetworking
     local total_pagers = 0
     function PagerChat:add_pager()
+	    local max_pagers_data = managers.player:has_category_upgrade("player", "corpse_alarm_pager_bluff") and tweak_data.player.alarm_pager.bluff_success_chance_w_skill or tweak_data.player.alarm_pager.bluff_success_chance
+	    local max_num_pagers = #max_pagers_data
+		for i, chance in ipairs(max_pagers_data) do
+			if chance == 0 then
+				max_num_pagers = i - 1
+
+				break
+			end
+		end
 		total_pagers = total_pagers +1
-		if sync and Net:IsHost() and not Global.game_settings.single_player then
-            managers.chat:send_message(ChatManager.GAME, managers.network.account:username() or "Offline", "Pagers Answered: "..total_pagers..".")
-	    elseif Net:IsClient() then
-	        managers.chat:_receive_message(ChatManager.GAME,"Pagers Answered (Since Joining)", total_pagers..".", Color('29A4F6'))
-		else
-		    managers.chat:_receive_message(ChatManager.GAME,"Pagers Answered", total_pagers..".", Color('29A4F6'))
-	    end
+	    managers.chat:_receive_message(ChatManager.GAME,"Pagers Answered", total_pagers .."/"..max_num_pagers, Color('29A4F6'))
 	end
+	
+	function PagerChat:sync_pagers(count)
+        total_pagers = count or 0
+    end
 end
